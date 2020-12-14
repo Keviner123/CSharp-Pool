d7ecd380f62ae4c24f47ab8e4b0988392bcf2445
My Name <my-name@chromium.org>
2020-12-14 12:42:49 +0100

    .

---
+++ Exercise3/Exercise1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hvad sker der med eksekveringstiden?
+            //Den var altid hurtigere i poolen. Specielt efter det nestede for loop
+
+            Stopwatch mywatch = new Stopwatch();
+            Console.WriteLine("Thread Pool Execution");
+            mywatch.Start();
+            ProcessWithThreadPoolMethod();
+            mywatch.Stop();
+            Console.WriteLine("Time consumed by ProcessWithThreadPoolMethod is : " + mywatch.ElapsedTicks.ToString());
+            mywatch.Reset();
+            Console.WriteLine("Thread Execution");
+            mywatch.Start();
+            ProcessWithThreadMethod();
+            mywatch.Stop();
+            Console.WriteLine("Time consumed by ProcessWithThreadMethod is : " + mywatch.ElapsedTicks.ToString());
+            Console.ReadKey();
+
+        }
+
+        static void ProcessWithThreadPoolMethod()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(Process);
+            }
+        }
+
+        static void ProcessWithThreadMethod()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Thread obj = new Thread(Process);
+                obj.Start();
+            }
+        }
+        static void Process(object callback)
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                for (int j = 0; j < 100000; j++)
+                {
+                }
+            }
+        }
+
+    }
+}
